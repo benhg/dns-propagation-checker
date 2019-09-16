015a1d4a13e8786e3de3a4120ad9b6f2c4423e71
Ben Glick <glick@lclark.edu>
2019-09-16 13:24:22 -0700

    clean and add sites

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ app.config["admin_email"] = "benjamin.glick@ge.com"
 app.secret_key = b'\x9b4\xf8%\x1b\x90\x0e[?\xbd\x14\x7fS\x1c\xe7Y\xd8\x1c\xf9\xda\xb0K=\xba'
 # I will obviously change this secret key before we go live
 
-svc_list = {"jupyter": "https://jupyter.datasci.watzek.cloud", "Datasci Home": "https://datasci.watzek.cloud", "Datasci RStudio": "https://rstudio.datasci.watzek.cloud"}
+svc_list = {
+    "jupyter": "https://jupyter.datasci.watzek.cloud",
+    "Datasci Home": "https://datasci.watzek.cloud",
+    "Datasci RStudio": "https://rstudio.datasci.watzek.cloud",
+    "status": "https://status.watzek.cloud",
+    "ezproxy": "https://library.lcproxy.org"}
+
 
 @app.route('/')
 @app.route('/index')
@@ -22,7 +28,7 @@ def hello_world():
     """Home Page"""
     status = {}
     for service in svc_list.keys():
-        status[service]= get_status(svc_list[service])
+        status[service] = get_status(svc_list[service])
 
     return render_template("status.html", status=status)
 
