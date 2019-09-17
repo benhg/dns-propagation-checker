92383af0bb64f477a95b5c5b47a60fe97ec4641c
Ben Glick <glick@lclark.edu>
2019-09-17 13:21:20 -0700

    even more elegance

--- cronjob_slackmessages.py
+++ cronjob_slackmessages.py
@@ -6,25 +6,10 @@ from socket import timeout
 import json
 import requests
 
+from common_stuff import get_status, svc_list
+
 slack_webhook_link = "https://hooks.slack.com/services/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"
 
-svc_list = {
-    "jupyter": "https://jupyter.datasci.watzek.cloud",
-    "Datasci Home": "https://datasci.watzek.cloud",
-    "Datasci RStudio": "https://rstudio.datasci.watzek.cloud",
-    "ezproxy": "https://library.lcproxy.org",
-    "William Stafford Archive":"http://williamstaffordarchives.org",
-    "AccessCeramics": "http://accessceramics.org",
-    "Oregon Poetic Voices": "http://oregonpoeticvoices.org",
-    "Watzek Server (watzek.lclark.edu)": "https://watzek.lclark.edu",
-    "Library Homepage (library.lclark.edu)": "https://library.lclark.edu",
-    "Vietnam Project": "https://vietnam.watzekdi.net",
-    "Senior Projects":"https://watzek.lclark.edu/seniorprojects/",
-    "LC Collaborative Research": "http://collaborativeresearch.lclark.edu",
-    "Special Collections": "http://specialcollections.lclark.edu",
-    "DataViz Server": "https://viz.datasci.watzek.cloud",
-    "LC PhysX": "https://lcphysx.lclark.edu",
-    }
 
 def check_statuses():
     """Home Page"""
@@ -36,18 +21,7 @@ def check_statuses():
 
 
 
-def get_status(service):
-    try:
-        response = urllib.request.urlopen(service, timeout=5).getcode()
-    except (HTTPError, URLError) as error:
-        return 'down'
-    except timeout:
-        return 'down'
-    else:
-        if response == 200:
-            return 'up'
-        else:
-            return 'down'
+
 
 if __name__ == '__main__':
     check_statuses()
