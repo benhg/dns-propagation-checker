87dd433c2b1fd5847d2acce693cf4fc3a9a9fcec
Ben Glick <glick@lclark.edu>
2019-09-26 13:31:31 -0700

    adding dns logic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import dns.resolver
 
 import json
 import urllib.request
@@ -10,7 +11,12 @@ svr_list = {
     "Google": "8.8.8.8",
     "GoDaddy": "ns1.domaincontrol.com",
     "LClark":"ns1.lclark.edu",
-    "Amazon":"",
+    "OpenDNS":"208.67.222.222",
+    "CloudFlare":"1.1.1.1",
+    "Norton ConnectSafe":"199.85.126.10",
+    "Comodo SecureDNS":"8.26.56.26",
+    "Quad9":"9.9.9.9",
+    "Verisign":"64.6.64.6",
     }
 
 
@@ -30,16 +36,25 @@ app.secret_key = b'\x9b4\xf8%\x1b\x90\x0e[?\xbd\x14\x7fS\x1c\xe7Y\xd8\x1c\xf9\xd
 @app.route('/login')
 @app.route('/status')
 def hello_world():
-    """Home Page"""
+    """Results page"""
+    if request.args.get("site", None):
+        site = request.args.get("site")
+    else:
+        return "Do that Again"
     status = {}
     for service in svr_list.keys():
-        status[service] = get_status(svc_list[service])
+        status[service] = get_status(svr_list[service], site)
 
     return render_template("status.html", status=status)
 
-def get_status(server, to=3):
-    return "resolves"
 
+
+
+def get_status(server, site):
+    my_resolver = dns.resolver.Resolver()
+    my_resolver.nameservers = [str(server)]
+    answer = my_resolver.query(site)
+    return answer
 
 
 
